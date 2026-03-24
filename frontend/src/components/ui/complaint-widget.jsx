14d2ae2bfee87e9fcd86bba8e0f3a768686343e7
zoaa3054 <zoaa3054@gmail.com>
2026-03-24 03:41:23 +0200

    chore: remove ComplaintWidget component and its dependencies from package.json and package-lock.json

--- frontend/src/components/ui/complaint-widget.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ComplaintRequestWidget } from "@zerotrace-solutions/complaint-request-widget";
-
-export function ComplaintWidget(props) {
-  return <ComplaintRequestWidget {...props} />;
-}
